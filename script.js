1463def4bd116b56ffb475809997a061f5d89a3e
JonasKaruzas <jonas.karuzas@gmail.com>
2022-09-14 21:41:07 +0300

    refactor: :art: changed to a global state obj

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const memoryObj = {
+const state = {
   num1: "",
   num2: "",
   operator: "",
@@ -10,8 +10,7 @@ function createUI(
   _container,
   clearAllHandler,
   clearCurrentHandler,
-  clickHandler,
-  _memoryObj
+  clickHandler
 ) {
   const buttonValues = [
     7,
@@ -45,7 +44,7 @@ function createUI(
   buttonValues.forEach((button) => {
     const buttonElement = document.createElement("button");
     buttonElement.innerText = button;
-    buttonElement.addEventListener("click", (e) => clickHandler(e, _memoryObj));
+    buttonElement.addEventListener("click", clickHandler);
     board.append(buttonElement);
   });
 
@@ -57,54 +56,56 @@ function createUI(
   clearAllBtn.innerText = "Clear All";
   clearCurrentBtn.innerText = "Clear Current";
 
-  clearAllBtn.addEventListener("click", () => clearAllHandler(_memoryObj));
-  clearCurrentBtn.addEventListener("click", () =>
-    clearCurrentHandler(_memoryObj)
-  );
+  clearAllBtn.addEventListener("click", clearAllHandler);
+  clearCurrentBtn.addEventListener("click", clearCurrentHandler);
 
   _container.append(board);
 }
 
-function setObjectInitialValues(_memoryObj) {
-  _memoryObj.num1 = "";
-  _memoryObj.num2 = "";
-  _memoryObj.operator = "";
-  _memoryObj.isFirstNumberInput = true;
+function setStateInitialValues() {
+  state.num1 = "";
+  state.num2 = "";
+  state.operator = "";
+  state.isFirstNumberInput = true;
 }
 
-function showOnDisplay(_memoryObj) {
+function showOnDisplay() {
   const display = document.getElementById("display");
-  if (_memoryObj.num1 === undefined) {
+  if (state.num1 === undefined) {
     display.innerText = "";
-  } else if (_memoryObj.result) {
-    display.innerText = _memoryObj.result;
+  } else if (state.result) {
+    display.innerText = state.result;
   } else {
-    display.innerText = `${_memoryObj.num1} ${_memoryObj.operator} ${_memoryObj.num2}`;
+    display.innerText = `${state.num1} ${state.operator} ${state.num2}`;
   }
 }
 
-function clearAll(_memoryObj) {
-  setObjectInitialValues(_memoryObj);
-  showOnDisplay(_memoryObj);
+function clearAll() {
+  setStateInitialValues();
+  showOnDisplay();
 }
 
-function clearCurrent(_memoryObj) {
-  _memoryObj.isFirstNumberInput
-    ? (_memoryObj.num1 = "")
-    : (_memoryObj.num2 = "");
+function clearCurrent() {
+  if (state.isFirstNumberInput) {
+    state.num1 = "";
+  } else {
+    state.num2 = "";
+  }
 
-  showOnDisplay(_memoryObj);
+  showOnDisplay();
 }
 
-function setOperator(operator, _memoryObj) {
-  _memoryObj.operator = operator;
-  _memoryObj.isFirstNumberInput = false;
+function setOperator(operator) {
+  state.operator = operator;
+  state.isFirstNumberInput = false;
 }
 
-function setNumber(number, _memoryObj) {
-  _memoryObj.isFirstNumberInput
-    ? (_memoryObj.num1 += number)
-    : (_memoryObj.num2 += number);
+function setNumber(number) {
+  if (state.isFirstNumberInput) {
+    state.num1 += number;
+  } else {
+    state.num2 += number;
+  }
 }
 
 function calculate(num1, num2, operator) {
@@ -129,31 +130,27 @@ function calculate(num1, num2, operator) {
   return result;
 }
 
-function getResult(_memoryObj) {
-  const result = calculate(
-    _memoryObj.num1,
-    _memoryObj.num2,
-    _memoryObj.operator
-  );
-  clearAll(_memoryObj);
-  _memoryObj.num1 = result;
+function getResult() {
+  const result = calculate(state.num1, state.num2, state.operator);
+  clearAll();
+  state.num1 = result;
 }
 
-function mainButtonClick(e, _memoryObj) {
+function numberAndOperatorClick(e) {
   const value = e.target.innerText;
   const operators = ["+", "-", "*", "/"];
 
   if (operators.includes(value)) {
-    setOperator(value, _memoryObj);
+    setOperator(value);
   } else if (value === "=") {
-    getResult(_memoryObj);
+    getResult();
   } else {
-    setNumber(value, _memoryObj);
+    setNumber(value);
   }
 
-  showOnDisplay(_memoryObj);
+  showOnDisplay();
 }
 
 const container = document.querySelector("#container");
 
-createUI(container, clearAll, clearCurrent, mainButtonClick, memoryObj);
+createUI(container, clearAll, clearCurrent, numberAndOperatorClick);
